c502535b7277111503b2c6a565c4827c1ae26ee6
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-09-04 13:02:57 +0600

    Payment Method Update

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { default: axios } = require("axios");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded());
 // --------------------------------------------------
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aeb0oh8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,7 +29,8 @@ async function run() {
 
     const productCollection = client.db("QuickShopBD").collection("AllProduct");
     const allUserCollection = client.db("QuickShopBD").collection("allUsers");
-
+    const paymentHistory = client.db("QuickShopBD").collection("payment");
+    const tnxId = new ObjectId().toString()
     // get all products
     app.get("/allProducts", async (req, res) => {
       const result = await productCollection.find().toArray();
@@ -55,6 +58,38 @@ async function run() {
       res.send(result)
     })
 
+
+    app.patch('/user-name-change/:email', async (req, res) => {
+      const email = req.params.email
+      const { name } = req.body
+      const filter = { email: email }
+      const option = { upsert: true }
+      const updateDoc = {
+        $set: {
+          name: name
+        }
+      }
+      const result = await allUserCollection.updateOne(filter, updateDoc, option)
+      res.send(result)
+    })
+
+
+    app.patch('/profile-update/:email', async (req, res) => {
+      const email = req.params.email
+      const update = req.body
+      const filter = { email: email }
+      const option = { upsert: true }
+      const updateDoc = {
+        $set: {
+          name: update.name,
+          address: update.address,
+          mobileNumber: update.mobileNumber
+        }
+      }
+      const result = await allUserCollection.updateOne(filter, updateDoc, option)
+      res.send(result)
+    })
+
     app.patch("/user-image-update/:email", async (req, res) => {
       const email = req.params.email
       const { image } = req.body
@@ -69,6 +104,86 @@ async function run() {
       res.send(result)
     })
 
+    // create payment getwaye
+    app.post('/create-payment', async (req, res) => {
+      // client data
+      const paymentInfo = req.body
+      // initateData for sslComarce
+      const initateData = {
+        store_id: "webwa66d6f4cb94fee",
+        store_passwd: "webwa66d6f4cb94fee@ssl",
+        total_amount: paymentInfo.amount,
+        currency: paymentInfo.currency,
+        tran_id: tnxId,
+        success_url: "http://localhost:8000/success-payment",
+        fail_url: "http://yoursite.com/fail.php",
+        cancel_url: "http://yoursite.com/cancel.php",
+        cus_name: "Customer Name",
+        cus_email: "cust@yahoo.com",
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        shipping_method: "NO",
+        product_name: "mobile",
+        product_category: "mobile",
+        product_profile: "general",
+        multi_card_name: "mastercard,visacard,amexcard",
+        value_a: "ref001_A&",
+        value_b: "ref002_B&",
+        value_c: "ref003_C&",
+        value_d: "ref004_D"
+      }
+      // post for asslcomarce
+      const response = await axios({
+        method: "POST",
+        url: "https://sandbox.sslcommerz.com/gwprocess/v4/api.php",
+        data: initateData,
+        headers: {
+          "content-type": "application/x-www-form-urlencoded"
+        }
+      })
+
+      const saveData = {
+        cus_name: "Dumy",
+        paymentId: tnxId,
+        amount: paymentInfo.amount,
+        status: "Pending"
+      }
+      // data save for mongodb database
+      const save = await paymentHistory.insertOne(saveData)
+      if (save) {
+        res.send({
+          paymentUrl: response.data.GatewayPageURL
+        })
+      }
+      // console.log(response)
+    })
+
+    app.post('/success-payment', async (req, res) => {
+      const successData = req.body
+      if (successData.status !== "VALID") {
+        throw new Error("Unauthorized payment, Invalid Payment")
+      }
+      // Update Database
+      const query = {
+        paymentId: successData.tran_id
+      }
+      const update = {
+        $set: {
+          status: "Success"
+        }
+      }
+
+      const updateData = await paymentHistory.updateOne(query, update)
+
+      console.log("success-data", successData)
+      console.log("Update-data", updateData)
+    })
 
 
 
